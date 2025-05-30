e2b72cb0831c2e72ae5d83128fb10c6ba97a9792
Mohit <mohitcod141@gmail.com>
2025-05-30 13:01:48 +0530

    reinitialize sequelize

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
@@ -37,6 +38,6 @@ Object.keys(db).forEach(modelName => {
 });
 
 db.sequelize = sequelize;
-
+db.Sequelize = Sequelize;
 
 module.exports = db;
